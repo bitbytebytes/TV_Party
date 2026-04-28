492dc490204b56095211c05bdc9a098209818f2d
bitbytebyts <bytebit@bitbytebytes.io>
2026-04-28 08:39:58 -0400

    implemented mml hooks. added trader task

--- Main.gd
+++ Main.gd
@@ -1,20 +1,51 @@
 extends Node
 
+const tv_path = "res://mods/TV_Party/Scenes/Television/Television_B_F"
+const task_path = "res://mods/TV_Party/Tasks/11_TV_Party.tres"
 
-const television_b = '\nconst Television_B = preload("res://mods/TV_Party/Scenes/Television/Television_B_F.tscn")'
-
+var _lib = null
 
 func _ready():
-    var db_script = load("res://Scripts/Database.gd")
-    var db_override_source = db_script.source_code
-    db_override_source += television_b
-    var db_override_script = GDScript.new()
-    db_override_script.source_code = db_override_source
-    db_override_script.reload()
-    var db = get_node("/root/Database")
-    db.set_script(db_override_script)
-    Database.ExecuteUpdate(true)
     
-    print("[TV Party] Television B added to master loot table")
+    if Engine.has_meta("RTVModLib"):
+        var lib = Engine.get_meta("RTVModLib")
+        if lib._is_ready():
+            _on_lib_ready()
+        else:
+            lib.frameworks_ready.connect(_on_lib_ready)
     
-    queue_free()
+
+func _on_lib_ready():
+    _lib = Engine.get_meta("RTVModLib")
+    _register_TV()
+
+
+func _register_TV():
+    var item_data = ResourceLoader.load(tv_path + ".tres", "", ResourceLoader.CACHE_MODE_REUSE)
+    if not item_data:
+        push_warning("[TV Party] failed to load " + tv_path + ".tres")
+        return
+        
+    if not _lib.register(_lib.Registry.ITEMS, "Television_B", item_data):
+        push_warning("[TV Party] Failed to register " + tv_path + ".tres")
+        return
+    #if not _lib.register(_lib.Registry.LOOT, "Television_B", { "item": item_data, "table":"LT_Master" }):
+        #push_warning("[TV Party] Failed to add Television B to master loot table")
+        #return
+    print("[TV Party] Television B item registered")
+    
+    if not _lib.register(_lib.Registry.SCENES, "Television_B", preload(tv_path + ".tscn")):
+        push_warning("[TV Party] Failed to register " + tv_path + ".tscn")
+        return
+    print("[TV Party] Television B scene registered")
+    
+    var task_data = ResourceLoader.load(task_path, "", ResourceLoader.CACHE_MODE_REUSE)
+    if not task_data:
+        push_warning("[TV Party] failed to load " + task_path)
+        return
+        
+    if not _lib.register(_lib.Registry.TRADER_TASKS, "11_TV_Party", { "task": task_data, "trader": "Generalist" }):
+        push_warning("[TV Party] Failed to register " + task_path)
+        return
+    print("[TV Party] TV Party task registered")
+    
